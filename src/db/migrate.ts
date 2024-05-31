fd6bedea556a91880560922adce38070a1411f67
Bartosz <bartek.104@o2.pl>
2024-05-31 18:49:24 +0200

    Working neon db

---
+++ src/db/migrate.ts
@@ -0,0 +1,20 @@
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import { migrate } from "drizzle-orm/neon-http/migrator";
+import { config } from "dotenv";
+
+config({ path: ".env.local" });
+
+const sql = neon(process.env.DATABASE_URL!);
+
+const db = drizzle(sql);
+
+const main = async () => {
+  try {
+    await migrate(db, { migrationsFolder: "drizzle" });
+    console.log("Migration complete");
+  } catch (e) {
+    console.error(e);
+  }
+};
+main();
