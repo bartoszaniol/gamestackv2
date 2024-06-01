79340ea793d172a84b2990a32e8c8faf059bf6a4
Bartosz <bartek.104@o2.pl>
2024-06-02 00:16:29 +0200

    Fixed tailwind problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,8 @@
-import db from "../db/db";
-import { user } from "../db/schema";
-
 export default async function Home() {
-  const data = await db.select().from(user);
   return (
-    <div>
-      <h1>Users</h1>
-      <ul>
-        {data.map((row) => (
-          <li key={row.id}>
-            <p>{row.name}</p>
-            <p>{row.description}</p>
-          </li>
-        ))}
-      </ul>
+    <div className="flex flex-row">
+      <div className="flex-[2]">Info</div>
+      <div className="flex-1">Log-in form</div>
     </div>
   );
 }
